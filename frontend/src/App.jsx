461df1cc6d14b60ad2155ba5060fa0095420415f
praise936 <peterpraise259@gmail.com>
2026-02-21 11:49:49 +0300

    case dit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Landing from './pages/landing'
+import Landing from './pages/Landing'
 import Navbar from './pages/Navbar'
 
 function App() {
